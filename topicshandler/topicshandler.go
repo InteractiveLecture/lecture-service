c5c314793f0d31cf8d6bf5631a1140a377074ab1
Rene Richter <Richterrettich@gmail.com>
2015-11-20 19:49:33 +0100

    Progress

--- topicshandler/topicshandler.go
+++ topicshandler/topicshandler.go
@@ -1,20 +1,87 @@
 package topicshandler
 
 import (
+	"encoding/json"
 	"net/http"
 
-	"gopkg.in/mgo.v2"
+	"github.com/gorilla/mux"
+	"github.com/richterrettich/lecture-service/models"
+	"github.com/richterrettich/lecture-service/paginator"
+	"github.com/richterrettich/lecture-service/repositories"
 )
 
-func NewCollectionHandler() http.Handler {
-	handlerFunc := func(w http.ResponseWriter, r *http.Request) {
+type errorHandler func(http.ResponseWriter, *http.Request) error
 
-		session, err := mgo.Dial("mongo")
-		session.DB("lecture-service").C("lectures")
+func (handler errorHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if err := handler(w, r); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
+
+func NewCollectionHandler(reposiroty repositories.TopicRepository) http.Handler {
+	handlerFunc := func(w http.ResponseWriter, r *http.Request) error {
+		pageRequest, err := paginator.ParsePages(r.URL)
+		if err != nil {
+			return err
+		}
+		defer reposiroty.Close()
+		result, err := repo.GetAll(pageRequest)
+		if err != nil {
+			return err
+		}
+		return json.NewEncoder(w).Encode(result)
 	}
 	return createHandler(handlerFunc)
 }
 
-func createHandler(handlerFunc http.HandlerFunc) http.Handler {
-	return http.Handler(http.HandlerFunc(handlerFunc))
+func NewFindHandler(repository repositories.TopicRepository) http.Handler {
+	handlerFunc := func(w http.ResponseWriter, r *http.Request) error {
+		vars := mux.Vars(r)
+		id := vars["id"]
+		defer repository.Close()
+		result, err := repo.GetOne(id)
+		if err != nil {
+			return err
+		}
+		return json.NewEncoder(w).Encode(result)
+	}
+	return createHandler(handlerFunc)
+}
+
+func NewCreateHandler(repository repositories.TopicRepository) http.Handler {
+	handlerFunc := func(w http.ResponseWriter, r *http.Request) error {
+		defer reposiroty.Close()
+		var topic models.Topic
+		err := json.NewDecoder(r.Body).Decode(topic)
+		if err != nil {
+			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+			return nil
+		}
+		err = topic.Validate()
+		if err != nil {
+			http.Error(w, "validation failed", http.StatusBadRequest)
+			return nil
+		}
+		return repository.Create(topic)
+	}
+	return createHandler(handlerFunc)
+}
+
+func NewUpdateHandler(repository repositories.TopicRepository) http.Handler {
+	handlerFunc := func(w http.ResponseWriter, r *http.Request) error {
+		defer repository.Close()
+		id := mux.Vars(r)["id"]
+		var newValues = make(map[string]interface{})
+		err := json.NewDecoder(r.Body).Decode(newValues)
+		if err != nil {
+			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+			return nil
+		}
+		return repository.Update(id, newValues)
+	}
+	return createHandler(handlerFunc)
+}
+
+func createHandler(handlerFunc errorHandler) http.Handler {
+	return http.Handler(errorHandler(handlerFunc))
 }
