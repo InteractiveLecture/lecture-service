488ff15777a417873ac25073e84507030064fbf7
Rene Richter <Richterrettich@gmail.com>
2016-01-05 16:27:23 +0100

    Progress

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+set -ev
+go test
 GO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o out/main .
-docker build -t interactive-lecture/lecture-service .
-
-cd sql
-./build.sh
-cd ..
+docker build -t openservice/lecture-service:latest .
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_REPO_SLUG}" = "InteractiveLecture/lecture-service" ] ; then
+  docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD" -e="$DOCKER_EMAIL"
+  docker push openservice/lecture-service:latest
+fi
