777fc5e0412a8195e90a357f41249c4f2798b233
Rene Richter <Richterrettich@gmail.com>
2016-01-08 15:51:48 +0100

    Fix CI-setup

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o out/main .
 docker build -t openservice/lecture-service:latest .
 cd integration-test
 docker-compose stop && docker-compose rm -f && docker-compose up -d
-sleep 30 && go test
+sleep 60 && go test
 cd ..
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_REPO_SLUG}" = "InteractiveLecture/lecture-service" ] ; then
   docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD" -e="$DOCKER_EMAIL"
